6e2bea1d07afc501f3a75f62ca261f9b22c48cdb
Sayman Rifat <saymanrifat@gmail.com>
2024-04-06 22:22:47 +0600

    add crypto and genesis block

--- block.js
+++ block.js
@@ -1,12 +1,26 @@
-class Block{
-    constructor(timestamp,prevHash,hash,data){
-        this.timestamp = timestamp;
-        this.prevHash = prevHash;
-        this.hash = hash;
-        this.data = data;
+const { GENESIS_DATA } = require("./config");
 
-    }
+class Block {
+  constructor({ timestamp, prevHash, hash, data }) {
+    this.timestamp = timestamp;
+    this.prevHash = prevHash;
+    this.hash = hash;
+    this.data = data;
+  }
+
+  static genesis() {
+    return new this(GENESIS_DATA);
+  }
 }
 
-const block1 = new Block('1/09/22','0xabc','0xc12','hello');
-console.log(block1)
+const block1 = new Block({
+  timestamp: "1/09/22",
+  prevHash: "0xabc",
+  hash: "0xc12",
+  data: "hello",
+});
+
+const genesisBlock = Block.genesis();
+console.log(genesisBlock);
+
+// console.log(block1);
---
+++ config.js
@@ -0,0 +1,8 @@
+const GENESIS_DATA = {
+  timeStamp: 1,
+  preHash: "0x000",
+  hash: "0x001",
+  data: [],
+};
+
+module.exports = { GENESIS_DATA };
